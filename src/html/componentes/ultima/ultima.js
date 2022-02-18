8612cb8f25a56068b5a9735252c6fcc3d709331c
bonafe <bonafe@opapagaiorinoceronte.net>
2022-02-18 10:29:24 -0300

    novo

--- src/html/componentes/ultima/ultima.js
+++ src/html/componentes/ultima/ultima.js
@@ -43,6 +43,8 @@ export class UltimaDAO extends EventTarget{
         }
 
         this.banco = undefined;
+        this.carregado = false;
+        this.atualizandoBanco = false;
         this.abrirBanco();
     }
 
@@ -50,7 +52,7 @@ export class UltimaDAO extends EventTarget{
 
     aguardarBanco(){
         return new Promise ((resolve, reject) => {
-            if (this.banco){
+            if (this.carregado){
                 resolve (true);
             }else{
                 this.addEventListener(UltimaDAO.EVENTO_BANCO_CARREGADO, ()=>{                    
@@ -137,44 +139,116 @@ export class UltimaDAO extends EventTarget{
             this.banco.onerror = evento => {                
                 console.error(`Erro no banco: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO}): ${evento.target.errorCode}`);
             };
-            this.bancoCarregado();
+
+            if (!this.atualizandoBanco){
+                this.bancoCarregado();
+            }
             return;
         };
 
 
-        // Este evento é implementado somente em navegadores mais recentes
+        
         request.onupgradeneeded = evento => {
+
+            this.atualizandoBanco = true;
 
             console.info (`Função de upgrade do banco: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO})`);
             this.banco = request.result;                        
 
-            Promise.all([
-                this.criarObjectStoreTelas(),
-                this.criarObjectStoreComponentes()
-            ]).then( valores => {
-                console.info (`Dados do banco foram inicializados: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO})`);
-                this.bancoCarregado();
-            });                                 
+            this.criarTabelas().then( retorno => {
+                this.adicionarElementos().then ( retorno => {
+                    console.info (`Dados do banco foram inicializados: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO})`);
+                    this.atualizandoBanco = false;
+                    this.bancoCarregado();
+                });
+            });                                       
         };
     }
+
+
+    criarTabelas(){
+        return new Promise((resolve, reject) => {
+
+            Promise.all([
+                new Promise ((resolveT, rejectT) => {
+                    console.info ("Criando ObjectStore de Telas");
+                    
+                    let object_store_telas = this.banco.createObjectStore("telas", { keyPath: "id", autoIncrement: true });
+                    
+                    object_store_telas.createIndex("index_descricao_tela", "descricao", { unique: false});    
+                    
+                    object_store_telas.transaction.oncomplete = evento => {
+                        resolveT(true);
+                    };
+                }),
+                new Promise((resolveC, rejectC) => {
+                    console.info ("Criando ObjectStore de Componentes");
+                    let object_store_componentes = this.banco.createObjectStore("componentes", { keyPath: "id", autoIncrement: true });
+
+                    object_store_componentes.createIndex("index_nome_componente", "nome", { unique: false});
+                                
+                    object_store_componentes.transaction.oncomplete = evento => {
+                        resolveC(true);
+                    };
+
+                })
+            ]).then(valores => {
+                resolve(true);
+            });
+        });
+    }
+
+    adicionarElementos(){
+        return new Promise((resolve, reject) => {
+            let transacao = this.banco.transaction (["telas","componentes"], "readwrite")
+
+            let osTelas = transacao.objectStore ("telas");            
+            osTelas.add (BaseTestesTreeMap.base.telas[0]);
+            
+            let osComponentes = transacao.objectStore ("componentes");      
+            BaseTestesTreeMap.base.componentes.forEach (componente => {
+                osComponentes.add (componente);
+            });
+
+            transacao.transaction.oncomplete = evento => {
+                console.info ("Elementos adicionados com sucesso");
+                resolve(true);
+            }      
+        });
+    }
+
 
 
 
     criarObjectStoreTelas(){
 
         return new Promise((resolve, reject) => {
+            console.info ("Criando ObjectStore de Telas");
             let object_store_telas = this.banco.createObjectStore("telas", { keyPath: "id", autoIncrement: true });
 
-            object_store_telas.createIndex("index_nome", "nome", { unique: false});
+            object_store_telas.createIndex("index_descricao_tela", "descricao", { unique: false});
     
             object_store_telas.transaction.oncomplete = evento => {
     
-                object_store_telas = this.banco.transaction ("telas", "readwrite").objectStore ("telas");
-                object_store_telas.add (BaseTestesTreeMap.base.telas[0]);
-                object_store_telas.transaction.oncomplete = evento => {
+                console.info ("ObjectStore Telas criada com sucesso. Adicionando telas...");
+                let transacao_telas = this.banco.transaction ("telas", "readwrite").objectStore ("telas");
+                transacao_telas.add (BaseTestesTreeMap.base.telas[0]);
+
+                transacao_telas.transaction.oncomplete = evento => {
+                    console.info ("Telas adicionadas com sucesso");
                     resolve(true);
                 }                                
             };   
+
+            object_store_telas.transaction.onerror = evento => {
+                console.error(`Erro ao adicionar telas: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO}): ${evento.target.errorCode}`);
+                reject("");
+            };
+
+            object_store_telas.transaction.onabort = evento => {
+                console.error(`Transação com telas abortada: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO}): ${evento.target.errorCode}`);
+                reject("");
+            };
         });   
     }
 
@@ -183,21 +257,30 @@ export class UltimaDAO extends EventTarget{
     criarObjectStoreComponentes(){
 
         return new Promise((resolve, reject) => {
+            console.info ("Criando ObjectStore de Componentes");
             let object_store_componentes = this.banco.createObjectStore("componentes", { keyPath: "id", autoIncrement: true });
 
-            object_store_componentes.createIndex("index_nome", "nome", { unique: false});
+            object_store_componentes.createIndex("index_nome_componente", "nome", { unique: false});
     
             object_store_componentes.transaction.oncomplete = evento => {
     
-                object_store_componentes = this.banco.transaction ("componentes", "readwrite").objectStore ("componentes");
+                console.info ("ObjectStore Componentes criada com sucesso. Adicionando componentes...");
+
+                let transacao_componentes = this.banco.transaction ("componentes", "readwrite").objectStore ("componentes");
                 
                 BaseTestesTreeMap.base.componentes.forEach (componente => {
-                    object_store_componentes.add (componente);
+                    transacao_componentes.add (componente);
                 });
 
-                object_store_componentes.transaction.oncomplete = evento => {
+                transacao_componentes.transaction.oncomplete = evento => {
+                    console.info ("Componentes adicionados com sucesso");
                     resolve(true);
-                }                                
+                }       
+                
+                transacao_componentes.transaction.onerror = evento => {
+                    console.error(`Erro ao adicionar componentes: ${UltimaDAO.NOME_BANCO} (versão: ${UltimaDAO.VERSAO}): ${evento.target.errorCode}`);
+                    reject("");
+                };
             };   
         });   
     }
