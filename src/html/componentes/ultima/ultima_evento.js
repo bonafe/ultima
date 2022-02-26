b44eec3293e7b10e0fa9547d51f242d16f56c12e
bonafe <bonafe@opapagaiorinoceronte.net>
2022-02-26 16:56:12 -0300

    novo

--- src/html/componentes/ultima/ultima_evento.js
+++ src/html/componentes/ultima/ultima_evento.js
@@ -6,6 +6,8 @@ export class UltimaEvento extends CustomEvent{
     static EVENTO_ATUALIZACAO_ELEMENTO = 'EVENTO_ATUALIZACAO_ELEMENTO';
     static EVENTO_ATUALIZACAO_VIEW = 'EVENTO_ATUALIZACAO_VIEW';
 
+    static EVENTO_EXECUTAR_ACAO = 'EVENTO_EXECUTAR_ACAO';
+
     static EVENTO_PLAYER_YOUTUBE_CARREGADO = 'EVENTO_PLAYER_YOUTUBE_CARREGADO';
 
     constructor(evento, objetoDeDados){
--- src/html/componentes/video/exibidor_video.js
+++ src/html/componentes/video/exibidor_video.js
@@ -5,7 +5,7 @@ import { UltimaEvento } from '../ultima/ultima_evento.js';
 export class ExibidorVideo extends ComponenteBase {
 
     constructor(){
-        super({templateURL:"/componentes/video/exibidor_video.html", shadowDOM:true});
+        super({templateURL:"/componentes/video/exibidor_video.html", shadowDOM:false});
 
         this._dados = undefined;
 
@@ -39,13 +39,56 @@ export class ExibidorVideo extends ComponenteBase {
         //Se todos os elementos estão prontos e nenhum componente de vídeo foi criado
         if (this.carregado && YT && this.dados && !this.componenteVideo){
             
+                this.inicializarAcoes();
+
                 this.componenteVideo = 
                     new YT.Player(
                         this.noRaiz.querySelector("#video"), 
                         {                  
-                            videoId: this.dados.src
+                            videoId: this.dados.src,
+                            events: {
+                                'onReady': evento =>{
+                                    evento.target.playVideo();
+                                },
+                                'onStateChange': evento =>{
+                                    this.renderizar();
+                                }
+                            }
                         }
-                    );                 
+                    );                
+        }
+
+        this.processarAcoes();
+    }
+
+
+
+    inicializarAcoes(){
+        if (this.dados.acoes){        
+            this.dados.acoes.forEach(acao => {
+                acao.executada = false;
+            });
+        }
+    }
+
+
+
+    processarAcoes(){
+        if (this.dados && this.componenteVideo){
+
+            //TODO: esse IF pode ir para a condição de cima? dá para garantir a ordem que vai acontecer em javascript?            
+            if (this.dados.acoes){ 
+
+                try{
+                    console.log (`Tempo Atual: ${this.componenteVideo.getCurrentTime()}`);
+                }catch(e){}
+                //this.tempoAtualVideo = this.componenteVideo.getCurrentTime();
+
+                this.dados.acoes.filter (acao => !acao.executada && (acao.tempo < this.tempoAtualVideo))
+                    .forEach(acao => {
+                        this.dispatchEvent (UltimaEvento.EVENTO_EXECUTAR_ACAO, acao);
+                    });                
+            }
         }
     }
 }
